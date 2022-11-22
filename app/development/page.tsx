7d675e020dd8af586efdd740945e6e8864be788b
dhara dave <imdavedhara@gmail.com>
2022-11-22 10:43:04 +0530

    commit code

---
+++ app/development/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./card";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return (
+    <>
+      <Card />
+    </>
+  );
+};
+
+export default Page;
