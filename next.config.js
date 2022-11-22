bf9f0a977575306a9aa2b595ce14c50f9eb3b625
dhara dave <imdavedhara@gmail.com>
2022-11-22 10:38:22 +0530

    commit code

--- next.config.js
+++ next.config.js
@@ -1,8 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  swcMinify: true,
   experimental: {
+    // Required:
     appDir: true,
   },
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+  images: {
+    domains: [
+      "assets.website-files.com",
+      "ik.imagekit.io",
+      "tse3.mm.bing.net",
+      "images.unsplash.com",
+      "media.istockphoto.com",
+      "images.pexels.com",
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
