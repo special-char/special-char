a599f8a75e9ce249edf789e03278f91f241ff180
Ankita Trivedi <iamtrivediankita@gmail.com>
2022-11-23 14:36:38 +0530

    page

--- app/development/page.tsx
+++ app/development/page.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-import Button from "./button";
-import Developercard from "./developercard";
-import Funfacts from "./funfacts";
-import Icon from "./icon";
+import Banner from "./banner";
+import Card from "./card";
 
 type Props = {};
 
 const Page = (props: Props) => {
   return (
     <>
-      <Button />
-      <Icon />
-      <Developercard />
-      <Funfacts />
+      {/* <Card /> */}
+      <Banner />
     </>
   );
 };
