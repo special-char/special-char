1caa3a309b80f1c2f482f7925589aca604547153
Lakshya Upadhyay <lakshyalove4904@gmail.com>
2022-11-29 13:58:30 +0530

    socialicons-bg

--- app/development/SocialIcon.tsx
+++ app/development/SocialIcon.tsx
@@ -3,28 +3,34 @@ import React from "react";
 import "@/styles/socialicon.css";
 import Icon from "@/ui/Icon";
 import Link from "next/link";
+import clsx from "clsx";
 type Props = {};
 
 const socialicon = [
   {
     icon: "linkedin",
     link: "https://www.linkedin.com",
+    className: "hover:bg-[#0a66c2]",
   },
   {
     icon: "instagram",
     link: "https://www.instagram.com",
+    className: "hover:bg-gradient-to-b from-[#c32aa3] via-[purple] to-[orange]",
   },
   {
     icon: "facebook",
     link: "https://www.facebook.com",
+    className: "hover:bg-[#1877f2]",
   },
   {
     icon: "twitter",
     link: "https://www.twitter.com",
+    className: "hover:bg-[#1da1f2]",
   },
   {
     icon: "dribbble",
     link: "https://www.dribbble",
+    className: "hover:bg-[#ea4c89]",
   },
 ];
 
@@ -32,14 +38,16 @@ const SocialIcon = (props: Props) => {
   return (
     <div className="socialicons">
       {socialicon.map((icon) => (
-        <span className="p-4 h-4 w-4 box-content rounded-full hover:bg-neutral-200 hover:scale-110 ease-in-out duration-300 hover:rounded-full hover:fill-neutral-400">
+        <span
+          className={clsx(
+            `p-4 h-4 w-4 box-content rounded-full hover:scale-110 ease-in-out duration-300 hover:rounded-full hover:fill-neutral-400`,
+            {
+              [icon.className]: true,
+            }
+          )}
+        >
           <Link key={icon.icon} href={icon.link} target="_blank">
-            <Icon
-              className=" hover:fill-neutral-400"
-              name={icon.icon}
-              height={16}
-              width={16}
-            />
+            <Icon className=" " name={icon.icon} height={16} width={16} />
           </Link>
         </span>
       ))}
