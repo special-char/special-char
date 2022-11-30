8cc94d8a59c274a20630056c99bffea657c032b2
Pruthvish Modi <Iampruthvishmodi@gmail.com>
2022-11-30 10:56:45 +0530

    unstage changes

--- app/development/banner.tsx
+++ app/development/banner.tsx
@@ -1,43 +1,20 @@
 import React from 'react';
-import '@/styles/banner.css';
+import '@/styles/Banner.css';
 
-type Props = {
-  data: Object;
-};
+type Props = {};
 
 const Banner = (props: Props) => {
   return (
-    <section id="banner" className="banner">
+    <section className="banner">
       <div className="banner__body">
-        <h1 className="banner__title">our</h1>
-        <span>work</span>
-        <h1>function</h1>
+        <h1>our</h1>
+        <span className="text-neutral-200 sm:text-8xl md:text-8xl lg:text-8xl ">
+          News
+        </span>
       </div>
-      <div className="banner__content">
-        <div className="flex flex-col gap-8">
-          <p>
-            "We are an award-winning branding and web agency committed to
-            excellence since forever."
-          </p>
-          <p>
-            "We set the standard of exceptional design creatively, technically,
-            sustainably, and ethically so that you can make the extraordinary
-            impact impact that you seek."
-          </p>
-        </div>
-        <button className="btn btn--primary">View on Designer</button>
-        <div className="flex flex-col gap-8"> </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Necessitatibus quo repudiandae odio dolores dignissimos velit labore
-          vitae aliquid. Nam, minima?
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero
-          facilis beatae ipsum iusto delectus consectetur quaerat explicabo
-          dolor distinctio ratione.
-        </p>
-        <button className="btn btn--primary">view on Designer</button>
+      <div className="banner__page">
+        <p>We share our thoughts on design. We design awesome things.</p>
+        <button className="btn btn--primary">Work With Us</button>
       </div>
     </section>
   );
