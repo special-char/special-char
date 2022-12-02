4b015e727d541a0fb6cb3c235ae78a3da316f505
Ankita Trivedi <iamtrivediankita@gmail.com>
2022-12-02 11:19:18 +0530

    about

--- app/development/aboutkofo.tsx
+++ app/development/aboutkofo.tsx
@@ -1,4 +1,4 @@
-import '@/styles/about.css';
+import '@/styles/aboutkofo.css';
 import Image from 'next/image';
 import React from 'react';
 
@@ -6,17 +6,17 @@ type Props = {};
 
 function Aboutkofo({}: Props) {
   return (
-    <section className="about">
-      <div className="about__image">
+    <section className="aboutkofo">
+      <div className="aboutkofo__image">
         <Image
           src="https://assets.website-files.com/6315d6de2357050021f26e96/63187ca7418405f870aba348_photo-4.jpg"
           alt="Hardwork"
           fill
         />
       </div>
-      <div className="about__content">
+      <div className="aboutkofo__content">
         <h2>About Kofo</h2>
-        <p className="about__p">
+        <p className="aboutkofo__p">
           We are an award-winning branding and web agency committed to
           excellence since forever.
           <br /> <br /> We set the standard of exceptional design creatively,
@@ -28,13 +28,13 @@ function Aboutkofo({}: Props) {
           <button className="btn btn--primary">View On Designer</button>
         </div>
       </div>
-      <Image
+      {/* <Image
         src="https://assets.website-files.com/6315d6de2357050021f26e96/63171d2704da6da8b4049353_sammy-line-paper-plane.svg"
         alt=""
         height={100}
         width={100}
         className="airplane"
-      />
+      /> */}
     </section>
   );
 }
